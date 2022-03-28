402fe3ca98c0237142d2029eb552ab5afe3b1f69
XenoZohar <xeno.zohar@gmail.com>
2022-03-28 12:04:24 +0200

    Add usings for controller for ease of use

--- SeidoDbWebApi/Controllers/NecklaceController.cs
+++ SeidoDbWebApi/Controllers/NecklaceController.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using NecklaceCRUD;
+using NecklaceDB;
+using NecklaceModels;
+
 namespace SeidoDbWebApi.Controllers
 {
     [Route("api/[controller]")]
